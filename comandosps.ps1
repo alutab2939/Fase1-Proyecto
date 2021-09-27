423f4fe450c8fb8143ee81d695c767609eb221b4
alutab2939 <91492963+alutab2939@users.noreply.github.com>
2021-09-27 20:28:59 +0200

    Update comandosps.ps1

--- comandosps.ps1
+++ comandosps.ps1
@@ -1,4 +1,14 @@
+#COMANDO PARA ABRIR POWERSHELL
+powershell
 #Comando para ver version NTF
 fsutil fsinfo ntfsinfo c:
 #Comando para ver interfaz de red
 Get-NetIPConfiguration
+#Para obtener el adaptador de red
+Get-NetAdapter
+#Para configurar la direccion IP 
+New-NetIPAddress -InterfaceIndex 1 -IPAddress 192.168.1.202 -PrefixLength 24 -DefaultGateway 192.168.1.201
+#Para configurar el DNS 
+Set-DnsClientServerAddress -InterfaceIndex 1 -ServerAddresses 192.168.1.201
+#Cambiar el nombre del equipo 
+Rename-Computer -NewName master-upv-es-r
